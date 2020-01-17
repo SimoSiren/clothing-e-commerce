d90f4b98ecd11c727746f40173fd2e2a3e5eb38f
Simo Siren <simo.siren@stud.pointcollege.fi>
2020-01-17 12:24:44 +0200

    sign-up componentti tehty, tyylit vielä tekemättä

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -1,5 +1,95 @@
 import React from 'react';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
-import { auth, createUSerProfileDocument } from '../../firebase/firebase-utils'
+import { auth, createUSerProfileDocument } from '../../firebase/firebase-utils';
 
+import './sign-up.styles.css';
+
+class SignUp extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            displayName: '',
+            email: '',
+            password: '',
+            confirmedPassword: ''
+        }
+    }
+
+    handleSubmit = async event => {
+        event.preventDefault();
+        const { displayName, email, password, confirmedPassword } = this.state;
+        if (password !== confirmedPassword) {
+            alert('passwords do not match');
+            return;
+        }
+        try {
+            const { user } = await auth.createUserWithEmailAndPassword(email, password);
+            await createUSerProfileDocument(user, { displayName });
+            this.setState({
+                displayName: '',
+                email: '',
+                password: '',
+                confirmedPassword: ''
+            })
+        }
+        catch (error) {
+            console.log(error);
+        }
+    };
+
+    //Yksi input-kentän handleChange käsittelijä kaikille kentille. setState dynaaminen:
+    //Kentän nimiarvoa vastaten sijoitetaan kentän value arvo
+    handleChange = event => {
+        const { name, value } = event.target;
+        this.setState({ [name]: value })
+    }
+
+    render() {
+        const { displayName, email, password, confirmedPassword } = this.state;
+        return (
+            <div className='sign-up'>
+                <h2 className='title'>I do not have an account yet</h2>
+                <span>Sign up with your email and password</span>
+                <form className='sign-up-form' onSubmit={this.handleSubmit}>
+                    <FormInput
+                        type='text'
+                        name='displayName'
+                        value={displayName}
+                        onChange={this.handleChange}
+                        label='Display Name'
+                        required
+                    />
+                    <FormInput
+                        type='email'
+                        name='email'
+                        value={email}
+                        onChange={this.handleChange}
+                        label='Email'
+                        required
+                    />
+                    <FormInput
+                        type='password'
+                        name='password'
+                        value={password}
+                        onChange={this.handleChange}
+                        label='Password'
+                        required
+                    />
+                    <FormInput
+                        type='password'
+                        name='confirmedPassword'
+                        value={confirmedPassword}
+                        onChange={this.handleChange}
+                        label='Confirm Password'
+                        required
+                    />
+                    <CustomButton type='submit'>Sign Up</CustomButton>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default SignUp;
